dffc6b54ec23152482f5ecf11a3e2d99e2cc9b7a
16-Shiva-16 <105637350+16-Shiva-16@users.noreply.github.com>
2022-11-17 13:52:36 +0530

    Add files via upload

---
+++ FINAL DELIVERABLES/APPLICATION BUILDING/app.py
@@ -0,0 +1,45 @@
+from gevent.pywsgi import WSGIServer
+import os
+
+
+import pickle
+import warnings
+
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn import metrics
+
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+file = open("model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+port=os.getenv('VCAP_APP_PORT','8080')
+
+if __name__ == "__main__":
+    app.secret_key=os.urandom(12)
+    app.run(debug=True,host='0.0.0.0',port=port)
